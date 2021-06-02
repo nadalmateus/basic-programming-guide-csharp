f65c08fbb82901d7b99962ae08cfcad7eb78504e
Mateus Nadal <nadalmateus@outlook.com>
2021-06-02 14:38:46 -0300

    Casting

---
+++ 02-casting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02_casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double b = 6.0;
+            double B = 8.0;
+            double h = 5.0;
+
+            double area = (b + B) / 2.0 * h;
+
+            Console.WriteLine(area);
+
+        }
+    }
+}
