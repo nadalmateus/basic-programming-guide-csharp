2bb275b483c9003b393473bca8b4e393c3f8b26c
Mateus Nadal <nadalmateus@outlook.com>
2021-06-07 14:19:14 -0300

    While

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace basic_programming_guide_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, soma;
+
+            x = int.Parse(Console.ReadLine());
+            soma = 0;
+            while (x != 0)
+            {
+                soma = soma + x;
+                x = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine(soma);
+
+
+
+
+        }
+    }
+}
